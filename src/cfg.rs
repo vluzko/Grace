396e8b451008aa3ed3e9fbba4ec7f6d7b8e64de2
Vincent Luczkow <vincent.luczkow@gmail.com>
2020-01-11 13:21:19 -0500

    Finished conversion of simple statements to cfg

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,8 +1,11 @@
 extern crate petgraph;
 
 use petgraph::Graph;
-use expression::{Assignment, Identifier, Node, Expr};
+use expression::{Identifier, Node, Module, Block, Stmt, Expr};
+use scoping::Context2;
 use typing::Type;
+
+type Cfg = Graph::<CfgVertex, i64>;
 
 pub struct CfgVertex {
     pub node_id: usize,
@@ -10,14 +13,90 @@ pub struct CfgVertex {
 }
 
 pub enum CfgStmt {
-    AssignmentStmt  {name: Identifier, expression: Node<Expr>},
-    LetStmt         {name: Identifier, expression: Node<Expr>},
-    ReturnStmt      (Node<Expr>),
-    YieldStmt       (Node<Expr>),
-    BranchStmt      (Node<Expr>),
+    Assignment  {name: Identifier, expression: Node<Expr>},
+    Let         {name: Identifier, expression: Node<Expr>},
+    Return      (Node<Expr>),
+    Yield       (Node<Expr>),
+    Branch      (Node<Expr>),
 }
 
-fn empty_cfg() -> Graph::<CfgVertex, i64> {
+fn empty_cfg() -> Cfg {
     let empty = Graph::<CfgVertex, i64>::new();
     return empty;
 }
+
+/// Generate a control-flow graph.
+// pub trait ToCfg {
+//     fn to_cfg(self: Self, context: &Context2, current: Cfg) -> Cfg {
+//         panic!()
+//     }
+// }
+
+// impl ToCfg for Node<Module> {
+//     fn to_cfg(self, context: &Context2, current: Cfg) -> Cfg {
+//         panic!()
+//     }
+// }
+
+impl Node<Block> {
+    fn to_cfg(self, context: &Context2, current: Cfg) -> Cfg {
+        let mut new_cfg = current;
+
+        for stmt in self.data.statements.into_iter() {
+            let val = match stmt.data {
+                Stmt::LetStmt{name, expression, ..} => {
+                    Node {
+                        id: stmt.id,
+                        scope: stmt.scope,
+                        data: CfgStmt::Let{name, expression}
+                    }
+                },
+                Stmt::AssignmentStmt{name, expression} => {
+                    Node {
+                        id: stmt.id,
+                        scope: stmt.scope,
+                        data: CfgStmt::Assignment{name, expression}
+                    }
+                },
+                Stmt::ReturnStmt(val) => {
+                    Node {
+                        id: stmt.id,
+                        scope: stmt.scope, 
+                        data: CfgStmt::Return(val)
+                    }
+                },
+                Stmt::YieldStmt(val) => {
+                    Node {
+                        id: stmt.id,
+                        scope: stmt.scope, 
+                        data: CfgStmt::Yield(val)
+                    }
+                },
+                Stmt::WhileStmt{condition, block} => {
+                    
+                    panic!()
+                },
+                _ => panic!()
+            };
+        }
+
+        return new_cfg;
+    }
+}
+
+// impl ToCfg for Node<Stmt> {
+//     fn to_cfg(self, context: &Context2, current: Cfg) -> Cfg {
+//         return match &self.data {
+//             Stmt::WhileStmt{ref condition, ref block} => {
+//                 panic!()
+//             },
+//             _ => panic!()
+//         };
+//     }
+// }
+
+// impl ToCfg for Node<Expr> {
+//     fn to_cfg(self, context: &Context2, current: Cfg) -> Cfg {
+//         panic!()
+//     }
+// }

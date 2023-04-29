0995ee290a0f4252cf8dab9b975d259383d8cecd
vluzko <vincent.luczkow@gmail.com>
2023-04-29 00:41:03 -0700

    Clippy for refinements

--- src/type_checking/refinements.rs
+++ src/type_checking/refinements.rs
@@ -27,46 +27,43 @@ pub fn check_constraints(scope_id: usize, context: &Context, constraints: Vec<Re
 
 fn call_from_refinement_type(
     scope_id: usize,
-    constraints: &Vec<Refinement>,
+    constraints: &[Refinement],
     context: &Context,
 ) -> (HashSet<Identifier>, Vec<String>) {
-    let mut full_constraints = constraints.clone();
+    let mut full_constraints = constraints.to_owned();
     let mut variables = HashSet::new();
     let mut checked = HashSet::new();
 
     let mut constraint_strings = vec![];
-    while full_constraints.len() > 0 {
+    while !full_constraints.is_empty() {
         let constraint = full_constraints.pop().unwrap();
         let (set, constraint_str) = constraint.construct_condition(context);
 
         for variable in set.iter() {
             if !checked.contains(variable) {
                 // Add the declaration to the constraints.
-                let var_dec_opt = context.get_declaration(scope_id, &variable);
-                match var_dec_opt {
-                    Ok(var_dec) => {
-                        let (dec_set, dec_str) = var_dec.construct_condition(context);
-                        checked = m_union(checked, dec_set.clone());
-                        variables = m_union(variables, dec_set);
-                        constraint_strings.push(format!("{} == {}", variable, dec_str));
+                let var_dec_opt = context.get_declaration(scope_id, variable);
+                if let Ok(var_dec) = var_dec_opt {
+                    let (dec_set, dec_str) = var_dec.construct_condition(context);
+                    checked = m_union(checked, dec_set.clone());
+                    variables = m_union(variables, dec_set);
+                    constraint_strings.push(format!("{} == {}", variable, dec_str));
 
-                        // Add the type to the constraints.
-                        let var_scope = context.get_declaring_scope(scope_id, &variable).unwrap();
-                        let var_type = context.get_type(scope_id, &variable).unwrap();
-                        match var_type {
-                            Type::Refinement(_, ref new_conds) => {
-                                let (new_idents, mut new_constraints) =
-                                    call_from_refinement_type(var_scope, new_conds, context);
-                                checked = m_union(checked, new_idents.clone());
-                                variables = m_union(variables, new_idents);
-                                constraint_strings.append(&mut new_constraints);
-                            }
-                            Type::i32 => {}
-                            x => panic!("Can't have a variable of type {:?} in a constraint", x),
+                    // Add the type to the constraints.
+                    let var_scope = context.get_declaring_scope(scope_id, variable).unwrap();
+                    let var_type = context.get_type(scope_id, variable).unwrap();
+                    match var_type {
+                        Type::Refinement(_, ref new_conds) => {
+                            let (new_idents, mut new_constraints) =
+                                call_from_refinement_type(var_scope, new_conds, context);
+                            checked = m_union(checked, new_idents.clone());
+                            variables = m_union(variables, new_idents);
+                            constraint_strings.append(&mut new_constraints);
                         }
-                        checked.insert(variable.clone());
+                        Type::i32 => {}
+                        x => panic!("Can't have a variable of type {:?} in a constraint", x),
                     }
-                    Err(_) => {}
+                    checked.insert(variable.clone());
                 }
             }
         }
@@ -74,7 +71,7 @@ fn call_from_refinement_type(
         variables = m_union(variables, set);
         constraint_strings.push(constraint_str);
     }
-    return (variables, constraint_strings);
+    (variables, constraint_strings)
 }
 
 /// Construct a call to Z3 using the Python API
@@ -84,12 +81,12 @@ trait ToPython {
 
 impl ToPython for CanModifyScope {
     fn construct_condition(&self, context: &Context) -> (HashSet<Identifier>, String) {
-        return match self {
+        match self {
             CanModifyScope::Statement(ref raw_stmt, _) => unsafe {
                 (**raw_stmt).construct_condition(context)
             },
             _ => panic!(),
-        };
+        }
     }
 }
 
@@ -98,25 +95,22 @@ impl ToPython for Refinement {
         let (l_set, l_str) = self.left.construct_condition(context);
         let (r_set, r_str) = self.right.construct_condition(context);
         let f_set = m_union(l_set, r_set);
-        return (
-            f_set,
-            format!("({}) {} ({})", l_str, self.operator.to_string(), r_str),
-        );
+        (f_set, format!("({}) {} ({})", l_str, self.operator, r_str))
     }
 }
 
 impl ToPython for Node<Stmt> {
     fn construct_condition(&self, context: &Context) -> (HashSet<Identifier>, String) {
-        return match &self.data {
+        match &self.data {
             Stmt::LetStmt { ref expression, .. } => expression.construct_condition(context),
             x => panic!("ToPython not implemented for {:?}", x),
-        };
+        }
     }
 }
 
 impl ToPython for Node<Expr> {
     fn construct_condition(&self, context: &Context) -> (HashSet<Identifier>, String) {
-        return match &self.data {
+        match &self.data {
             Expr::BinaryExpr {
                 ref operator,
                 ref left,
@@ -137,6 +131,6 @@ impl ToPython for Node<Expr> {
             }
             Expr::Int(value) | Expr::Float(value) => (HashSet::new(), value.clone()),
             x => panic!("Not a legal constraint: {:?}", x),
-        };
+        }
     }
 }
